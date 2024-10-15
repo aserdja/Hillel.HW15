abc0962aca36a02d9deee91087f79fe82ef3f5b3
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-10-15 20:18:15 +0300

    Created models

---
+++ HW15/HW15.DAL/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace HW15.DAL.Models
+{
+	public class Author
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public DateOnly DateOfBirth { get; set; }
+
+		public ICollection<Book>? Books { get; set; }
+	}
+}
---
+++ HW15/HW15.DAL/Models/Genre.cs
@@ -0,0 +1,10 @@
+﻿namespace HW15.DAL.Models
+{
+	public class Genre
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+
+		public ICollection<Book>? Books { get; set; }
+	}
+}
